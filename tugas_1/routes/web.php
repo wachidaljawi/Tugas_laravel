5874fc1dc476b4a323e51306fbb02b018fd2bc80
wachidaljawi <rwachid17@gmail.com>
2020-04-14 22:58:39 +0700

    Memasukkan file bootsrap

--- tugas_1/routes/web.php
+++ tugas_1/routes/web.php
@@ -11,8 +11,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('importir')->group(function () {
+    Route::get('/karyawan', function () {
+        $karyawan = ['Erik jayadi', 'Ujang', 'Urip', 'Merip', 'Kurip'];
+        $staff = ['Ani', 'Una', 'Kiki', 'Ari', 'Mantap'];
+        $kontrak = ['Ina', 'Firman', 'Supri', 'Andi', 'Bedah'];
+        return view('pegawai.karyawan', compact('karyawan'));
+    });
 });
